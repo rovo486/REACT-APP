8a325aa4b12025ee237fd30ece9cbc9085e96754
rovo486 <rovo486@gmail.com>
2020-05-14 18:05:35 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+      <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+    <nav>
+        <ul>
+            <li><a href="1.html">html</a></li>
+            <li><a href="2.html">css</a></li>
+            <li><a href="3.html">javascript</a></li>
+        </ul>
+    </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Subject title="WEB" sub="world wide web!"></Subject>
+     <TOC></TOC>
+     <Content title="HTML" desc="html is hypertext Markup Language"></Content>
     </div>
   );
 }
